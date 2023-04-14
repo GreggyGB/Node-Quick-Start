152e52ccdafa55e27b2b46e69538ab76492433a0
GreggyGB <43723411+GreggyGB@users.noreply.github.com>
2023-04-14 18:08:33 +0100

    3 nodes setup

--- equilibria.sh
+++ equilibria.sh
@@ -21,10 +21,11 @@ install_node () {
   sudo cp ~/Equilibria/eqnode.service /etc/systemd/system/
   sudo systemctl daemon-reload
   sudo systemctl enable eqnode.service
+  cp ~/bin/daemon ~/bin/xeq1
 }
 
 prepare_sn () {
-  ~/bin/daemon prepare_sn
+  ~/bin/xeq1 prepare_sn
 }
 
 start () {
@@ -33,13 +34,22 @@ start () {
 }
 
 status () {
-  ~/bin/daemon status
+  bash equilibria.sh status && bash equilibria2.sh status && bash equilibria3.sh status
   #systemctl status eqnode.service
 }
 
 stop_all_nodes () {
-  echo Stopping XEQ node
+  echo Stopping XEQ nodes
   sudo systemctl stop eqnode.service
+  sudo systemctl stop eqnode2.service
+  sudo systemctl stop eqnode3.service
+}
+
+start_all () {
+  echo Starting XEQ nodes 0-3.
+  sudo systemctl start eqnode.service
+  sudo systemctl start eqnode2.service
+  sudo systemctl start eqnode3.service
 }
 
 log () {
@@ -51,7 +61,12 @@ update () {
 }
 
 print_sn_key () {
-  ~/bin/daemon print_sn_key
+  echo xeq1 key
+  ~/bin/xeq1 --rpc-bind-port 9231 print_sn_key
+  echo xeq2 key
+  ~/bin/xeq2 --rpc-bind-port 9241 print_sn_key
+  echo xeq3 key
+  ~/bin/xeq3 --rpc-bind-port 9251 print_sn_key
 }
 
 fork_update () {
@@ -63,8 +78,15 @@ fork_update () {
   sudo systemctl stop eqnode.service
   rm -r ~/bin
   cd build/Linux/_HEAD_detached_at_v17.1.0_/release && mv bin ~/
+  cp ~/bin/daemon ~/bin/xeq1
+  cp ~/bin/daemon ~/bin/xeq2
+  cp ~/bin/daemon ~/bin/xeq3
   sudo systemctl enable eqnode.service
   sudo systemctl start eqnode.service
+  sudo systemctl enable eqnode2.service
+  sudo systemctl start eqnode2.service
+  sudo systemctl enable eqnode3.service
+  sudo systemctl start eqnode3.service
 }
 
 case "$1" in
@@ -76,5 +98,7 @@ case "$1" in
   log ) log ;;
   update ) update ;;
   fork_update ) fork_update ;;
+  status_all ) status_all_nodes ;;
+  start_all ) start_all ;;
   print_sn_key ) print_sn_key ;;
 esac
